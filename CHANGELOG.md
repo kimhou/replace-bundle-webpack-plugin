e4842f78e51fd8118199d7441cf7de0d0d05d9c2
kimhou <kimhou.life@gmail.com>
2016-02-02 17:30:23 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## v0.0.3
+* Correct string type check (@mhodgson)
+
+## v0.0.2
+ * Preserve loader "this" context for "replacement" function (@mdreizin)
+
+## v0.0.1 - Initial Release
---
+++ loader.js
@@ -0,0 +1,28 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author James Andersen @jandersen78
+*/
+var loaderUtils = require("loader-utils");
+var StringReplacePlugin = require('./index.js');
+
+module.exports = function(source) {
+    var id = loaderUtils.parseQuery(this.query).id;
+
+    var stringReplaceOptions = this.options[StringReplacePlugin.REPLACE_OPTIONS];
+    if(!stringReplaceOptions.hasOwnProperty(id)) {
+        this.emitWarning('no replacement options found for id ' + id);
+    } else {
+        var options = stringReplaceOptions[id];
+
+        if(typeof source === "string") {
+            options.replacements.forEach(function(repl) {
+                source = source.replace(repl.pattern, repl.replacement.bind(this));
+            }, this);
+        } else {
+            this.emitWarning("'source' received by loader was not a string");
+        }
+    }
+
+	this.cacheable && this.cacheable();
+	return source;
+};
